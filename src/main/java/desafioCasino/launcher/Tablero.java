21f639f75d3381425b3854aed3eef2b726d5c132
Jorge <jorge.quidel312@gmail.com>
2022-11-13 20:35:09 -0300

    elegir juego desde launcher

--- src/main/java/desafioCasino/launcher/Tablero.java
+++ src/main/java/desafioCasino/launcher/Tablero.java
@@ -1,9 +1,27 @@
 package desafioCasino.launcher;
 
 import desafioCasino.juegos.Blackjack;
+import desafioCasino.juegos.Carioca;
+import desafioCasino.juegos.Escoba;
+import desafioCasino.juegos.Poker;
+import desafioCasino.utilidades.Utilidad;
 
 public class Tablero {
     public static void main(String[] args) {
-        new Blackjack().jugar();
+        elegirJuego();
+    }
+    public static void elegirJuego(){
+        bucle:
+        while(true){
+            System.out.print("Que quiere jugar?\n[1].Blackjack\n[2].Poker\n[3].Carioca\n[4].Escoba\n[0].Salir\n>");
+            switch (Utilidad.pedirOpcionEntera()) {
+                case 0 -> {System.out.println("Hasta pronto");break bucle;}
+                case 1 -> new Blackjack().jugar();
+                case 2 -> new Poker().jugar();
+                case 3 -> new Carioca().jugar();
+                case 4 -> new Escoba().jugar();
+                default -> System.out.println("Ingrese una de las opciones");
+            }
+        }
     }
 }
